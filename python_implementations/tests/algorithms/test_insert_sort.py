ee92f7d8ce6a7d02d45f5d9243f1f8ed946946c1
Joao Junior <joao.junior@shopify.com>
2021-03-08 20:57:07 -0500

    Add the insert sort algorithm

---
+++ python_implementations/tests/algorithms/test_insert_sort.py
@@ -0,0 +1,30 @@
+import pytest
+
+from algorithms.sorting.insert_sort import InsertSort
+
+
+@pytest.fixture
+def insert_sort():
+    return InsertSort()
+
+
+def test_array_already_sorted_asc(insert_sort):
+    items = [0, 1, 2, 3, 4]
+
+    insert_sort.sort(items)
+    assert list(range(5)) == items
+
+
+def test_array_sorted_desc(insert_sort):
+    items = [4, 3, 2, 1, 0]
+
+    insert_sort.sort(items)
+    assert list(range(5)) == items
+
+
+def test_array_sorted_random(insert_sort):
+    items = [0, 4, 2, 3, 1]
+
+    assert list(range(5)) != items
+    insert_sort.sort(items)
+    assert list(range(5)) == items

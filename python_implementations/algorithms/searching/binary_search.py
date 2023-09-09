d82e2310f6d8ee0307201089750de9ae0a0825c0
Joao Junior <joaojunior@Joaos-MBP.localdomain>
2023-09-09 12:32:00 -0400

    Adding binary search algorithm to find the bigger item less than the given item. It returns -1 when is not possible find it

--- python_implementations/algorithms/searching/binary_search.py
+++ python_implementations/algorithms/searching/binary_search.py
@@ -10,3 +10,17 @@ def binary_search(item, items):
         else:
             right = mid - 1
     return -1
+
+
+def binary_search_less_than(item, items):
+    left = 0
+    right = len(items) - 1
+    idx = -1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if items[mid] < item:
+            idx = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+    return idx
--- python_implementations/tests/algorithms/searching/test_binary_search.py
+++ python_implementations/tests/algorithms/searching/test_binary_search.py
@@ -1,4 +1,5 @@
-from algorithms.searching.binary_search import binary_search
+from algorithms.searching.binary_search import (binary_search,
+                                                binary_search_less_than)
 
 
 class TestBinarySearch:
@@ -16,3 +17,22 @@ class TestBinarySearch:
 
         assert flag == binary_search(less_than_min, items)
         assert flag == binary_search(bigger_than_max, items)
+
+
+class TestBinarySearchLessThan:
+    def test_found_bigger_item_less_than_given_item(self):
+        items = list(range(10))
+
+        for idx in range(1, len(items)):
+            item = items[idx]
+            assert idx - 1 == binary_search_less_than(item - 0.1, items)
+            assert idx - 1 == binary_search_less_than(item, items)
+            assert idx == binary_search_less_than(item + 0.1, items)
+
+    def test_return_flag_when_given_element_is_min_or_less(self):
+        items = list(range(10))
+        item = min(items)
+        flag = -1
+
+        assert flag == binary_search_less_than(item - 0.1, items)
+        assert flag == binary_search_less_than(item, items)
